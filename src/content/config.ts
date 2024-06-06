cad712fd1e8aee4d7d01006e8c488a5081c5fd45
Suzuka Takeuchi <sztk1@nifty.com>
2024-06-06 09:08:51 +0900

    content collectiona and view transitiona

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+// import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { posts: postCollection };
